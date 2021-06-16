849d6583ad77ef75bb0bd24998dfaac6d539de32
Paul Wroe <54@live.co.uk>
2021-06-16 23:25:12 +0100

    Updated Base URL

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'thedaisyclinic.epizy.com';
+	public $baseURL = 'thedaisyclinic.epizy.com/';
 
 	/**
 	 * --------------------------------------------------------------------------
